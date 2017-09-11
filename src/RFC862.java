0ee41abbe0d599fd02835f2e5f5916da02416622
Hjalte Juel Skovrup <hjsk@itu.dk>
2017-09-11 16:40:30 +0200

    Upload of the first assignment

---
+++ src/RFC862.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+import java.net.*;
+import java.io.*;
+public class RFC862{
+    public static void main(String args[]){
+        DatagramSocket aSocket = null;
+        try{
+            aSocket = new DatagramSocket(7007);
+            // create socket at agreed port
+            byte[] buffer = new byte[1000];
+            while(true){
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                aSocket.receive(request);
+                DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(),
+                        request.getAddress(), request.getPort());
+                System.out.println("UDP packet from: " + new String(request.getAddress().toString()));
+                aSocket.send(reply);
+            }
+        }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
+        }catch (IOException e) {System.out.println("IO: " + e.getMessage());
+        }finally {if(aSocket != null) aSocket.close();}
+    }
+}
