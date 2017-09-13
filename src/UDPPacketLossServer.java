c38b24386a952b221cad9e43bc233a3ae1dff57f
Jakob Roos <jroo@itu.dk>
2017-09-13 11:55:12 +0200

    Kom kom

--- src/UDPPacketLossServer.java
+++ src/UDPPacketLossServer.java
@@ -1,3 +1,5 @@
+import sun.misc.Queue;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -8,23 +10,55 @@ import java.net.SocketException;
  */
 public class UDPPacketLossServer {
     public static void main(String args[]){
-        DatagramSocket aSocket = null;
-        try{
-            aSocket = new DatagramSocket(7007);
             // create socket at agreed port
+        Queue<DatagramPacket> pq = new Queue<DatagramPacket>();
+            Thread recieveThread = new Thread(new Runnable() {
+                @Override
+            public void run() {
+                    DatagramSocket aSocket = null;
+                    try {
+                        aSocket = new DatagramSocket(7007);
+                        while(true){
+                            byte[] buffer = new byte[1000];
+                            DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                            aSocket.receive(request);
+                            DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(),
+                                    request.getAddress(), 7009);
+                            pq.enqueue(reply);
 
-            while(true){
-                byte[] buffer = new byte[1000];
-                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-                aSocket.receive(request);
-                DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(),
-                        request.getAddress(), 7009);
-                System.out.println("UDP packet from: " + new String(request.getAddress().toString()));
-                aSocket.send(reply);
+                        }
+                    }
+                    catch (SocketException e1) {
+                        e1.printStackTrace();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+
+            });
+        Thread replyThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                DatagramSocket replySocket = null;
+                try {
+                    replySocket = new DatagramSocket(7009);
+                    while (true) {
+                        if(!pq.isEmpty()) {
+                            DatagramPacket dp = pq.dequeue();
+                            System.out.println("UDP packet from: " + new String(dp.getAddress().toString()));
+                            replySocket.send(dp);
+                        }
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
-        }catch (IOException e) {System.out.println("IO: " + e.getMessage());
-        }finally {if(aSocket != null) aSocket.close();}
-    }
+            });
+        recieveThread.start();
+        replyThread.start();
+        }
 }
 
