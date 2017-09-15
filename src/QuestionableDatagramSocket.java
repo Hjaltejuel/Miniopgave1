cf1297955f03a50a257d887e07e56dc1033bbb3a
Hjaltejuel <hjsk@itu.dk>
2017-09-15 23:37:41 +0200

    finished second assignment

--- src/QuestionableDatagramSocket.java
+++ src/QuestionableDatagramSocket.java
@@ -9,8 +9,8 @@ import java.util.Random;
  * Created by Hjalte on 10-09-2017.
  */
 public class QuestionableDatagramSocket extends DatagramSocket {
+    //Saves the datagram to be reversed
     DatagramPacket saved = null;
-    boolean reverse = false;
 
     public QuestionableDatagramSocket() throws SocketException {
         super();
@@ -18,52 +18,48 @@ public class QuestionableDatagramSocket extends DatagramSocket {
 
     @Override
     public void send(DatagramPacket a) {
-        if (this.reverse == true) {
-            reverse(a);
-            reverse = false;
+        //If we are on a second datagram in a reverse series
+        if (saved!=null) {
+            try {
+                //send the datagrams in reverse order
+                super.send(a);
+                super.send(saved);
+                saved = null;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         } else {
+            //50% chance to do something with the datagramPacket
             Random random = new Random();
             if (random.nextBoolean() == true) {
                 int choosen = random.nextInt(3);
+                //33% chance to do either a discard, reorder or a duplicate
                 if (choosen == 0) {
-                    System.out.println("I DISCARD");
-                    //discard
+                    //do nothing
                 } else if (choosen == 1) {
                     try {
-                        System.out.println("I DUPLICATE");
+                        //Send twice
                         super.send(a);
                         super.send(a);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    //NOTE: reverse will only send if 2 of more datagrams are being send, as it needs something to reverse the order with
+                    //If less than 2 datagrams are send and we are supposed to reverse, reverse will work as a discard
                 } else if (choosen == 2) {
-                    System.out.println("I REVERSE");
-                    reverse(a);
-                    this.reverse = true;
+                    //look for reverse
+                    saved = a;
                 }
+
             } else try {
+                //If none of the above actions are triggered send as normal
                 super.send(a);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
-    }
-
-    public void reverse(DatagramPacket a) {
-        if (saved == null) {
-            saved = a;
-        } else {
-            try {
-                System.out.println("SENDING REVERSE");
-                super.send(a);
-                super.send(saved);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            saved = null;
-        }
     }
 }
 
