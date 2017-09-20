dbbf3301cc0db2bc5573c1533bc19c1651a83047
Jakob Roos <jroo@itu.dk>
2017-09-20 08:50:45 +0200

    Kom kom

--- src/RFC862.java
+++ src/RFC862.java
@@ -2,28 +2,25 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
-
-import java.net.*;
-import java.io.*;
 public class RFC862{
     public static void main(String args[]){
-        DatagramSocket aSocket = null;
+        DatagramSocket dataSocket = null;
         try{
-            aSocket = new DatagramSocket(7007);
-            // create socket at agreed port
+            dataSocket = new DatagramSocket(7007);
+
             byte[] buffer = new byte[1000];
             while(true){
-                //Create a packet for the recieve
+
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-                aSocket.receive(request);
+                dataSocket.receive(request);
                 DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(),
                         request.getAddress(), request.getPort());
-                //Send the packet back
+
                 System.out.println("UDP packet from: " + new String(request.getAddress().toString()));
-                aSocket.send(reply);
+                dataSocket.send(reply);
             }
         }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
         }catch (IOException e) {System.out.println("IO: " + e.getMessage());
-        }finally {if(aSocket != null) aSocket.close();}
+        }finally {if(dataSocket != null) dataSocket.close();}
     }
 }
