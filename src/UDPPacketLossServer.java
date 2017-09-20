1ad180585db47c46844c232b074dd39f578996b0
Thomas Andersen <thta@itu.dk>
2017-09-20 08:50:32 +0200

    Refactored UDPPacketLossServer

--- src/UDPPacketLossServer.java
+++ src/UDPPacketLossServer.java
@@ -10,59 +10,47 @@ import java.net.SocketException;
  */
 public class UDPPacketLossServer {
     public static void main(String args[]){
-        //Create a queue for the packages
-        Queue<DatagramPacket> pq = new Queue<DatagramPacket>();
-        //Make a new thread for recieving
-            Thread recieveThread = new Thread(new Runnable() {
-                @Override
-            public void run() {
-                    DatagramSocket aSocket = null;
-                    try {
-                        aSocket = new DatagramSocket(7007);
+        Queue<DatagramPacket> packetQueue = new Queue<>();
 
-                        while(true){
-                            //Recieve, make reply, add it to queue.
-                            byte[] buffer = new byte[10000];
-                            DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-                            aSocket.receive(request);
-                            DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(),
-                                    request.getAddress(), 7009);
-                            pq.enqueue(reply);
-
-                        }
-                    }
-                    catch (SocketException e1) {
-                        e1.printStackTrace();
-                    } catch (IOException e1) {
-                        e1.printStackTrace();
-                    }
-                }
-
-            });
-        //Create thread for sending
-        Thread replyThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                DatagramSocket replySocket = null;
+            Thread receiveThread = new Thread(() -> {
                 try {
-                    replySocket = new DatagramSocket();
-                    //Keep taking from the priority queue, and sending the packet
-                    while (true) {
-                        if(!pq.isEmpty()) {
-                            DatagramPacket dp = pq.dequeue();
-                            replySocket.send(dp);
-                        }
-                    }
+                    DatagramSocket receiveSocket = new DatagramSocket(7007);
 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    while(true){
+                        //Receive, make reply, add it to queue.
+                        byte[] buffer = new byte[10000];
+                        DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                        receiveSocket.receive(request);
+                        DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(),
+                                request.getAddress(), 7009);
+                        packetQueue.enqueue(reply);
+
+                    }
                 }
-            }
+                catch (SocketException e1) {
+                    e1.printStackTrace();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             });
-        //Start the two threads
-        recieveThread.start();
+
+        Thread replyThread = new Thread(() -> {
+            try {
+                DatagramSocket replySocket = new DatagramSocket();
+                //Keep taking from the priority queue, and sending the packet
+                while (true) {
+                    if(!packetQueue.isEmpty()) {
+                        DatagramPacket packet = packetQueue.dequeue();
+                        replySocket.send(packet);
+                    }
+                }
+
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        receiveThread.start();
         replyThread.start();
         }
 }
